5faf50f0557165c4e05190d7f9f063ab2dd37534
Sergey Pisarko <ravenheart.sp@gmail.com>
2023-06-20 10:14:59 +0300

    fix:

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const articles = document.querySelectorAll('.article-big');
 for (let article of articles) {
-    const img = article.querySelector('img');
+    //const img = article.querySelector('img');
     const text = article.querySelector('div > p');
+    
+    async function giveText() {
 
-    giveText();
-}
+        const url = ('https://baconipsum.com/api/?' + new URLSearchParams({
+            type: "all-meat", 
+            sentences: 5,
+            format: 'text'
+        }))
 
-async function giveText() {
-    const url = ('https://baconipsum.com/api/?' + new URLSearchParams({
-        type: "all-meat", 
-        sentences: 5,
-        format: 'text'
-    }))
+        const result = await fetch(url)
+        .then(response => response.text())
+        .then(data => text.textContent = data)
+    }
 
-    const result = await fetch(url)
-    .then((response) => response.text())
-    .then((data) => text.textContent = data);  
 }

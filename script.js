0b34694adcc67e1049aea0b02b7fbc621930ba72
Sergey Pisarko <ravenheart.sp@gmail.com>
2023-06-20 09:35:05 +0300

    fix: clear comments

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const articles = document.querySelectorAll('.article-big');
 for (let article of articles) {
     const img = article.querySelector('img');
     const text = article.querySelector('div > p');
-    // {
-    //     "type": "all-meat",
-    //     "sentences": 3,
-    //     "format": "text"    
-    // }
+    
     fetch('https://baconipsum.com/api/?' + new URLSearchParams({
         type: "all-meat", 
         sentences: 5,

d191d62ce0285b7a61d364b5698ab3136b2ef91f
Sergey Pisarko <ravenheart.sp@gmail.com>
2023-06-20 09:28:45 +0300

    fix: fetch

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const articles = document.querySelectorAll('.article-big');
 for (let article of articles) {
     const img = article.querySelector('img');
     const text = article.querySelector('div > p');
-
-    fetch('https://baconipsum.com/api/?callback=?')
-    .then((response) => {
-        return response.text();
-    })
-    .then((data) => {
-        text.textContent = data;
-    });
+    // {
+    //     "type": "all-meat",
+    //     "sentences": 3,
+    //     "format": "text"    
+    // }
+    fetch('https://baconipsum.com/api/?' + new URLSearchParams({
+        type: "all-meat", 
+        sentences: 5,
+        format: 'text'
+    }))
+    .then((response) => response.text())
+    .then((data) => text.textContent = data);
 }
